e4f4494c712393f2b56516315576c0bce1f0210c
Varun Sharman <varunsharman@Varuns-MacBook-Pro.local>
2023-05-10 08:56:04 +0200

    added coordinates and migrations

--- ranking/views.py
+++ ranking/views.py
@@ -11,14 +11,27 @@ def index(request):
     aoi_id = request.GET.get('aoi_id')
     # TODO Populate localData - we could hardcode it, do we really need a DB/file?
 
-
     # Hardcoded - will be retrieved from Group 5 API
-    subareas = [{'geometry': 12, 'centroid': {'x': '1', 'y': '2'}}, {'geometry': 23, 'centroid': {'x': '2', 'y': '3'}}]
+    subareas = [{'geometry': {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
+            [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
+             [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]
+        ]
+    }, 'centroid': {'x': '1', 'y': '2'}}, {'geometry':  {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
+            [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
+             [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]
+        ]
+    }, 'centroid': {'x': '2', 'y': '3'}}]
 
     # url = "https://api.n2yo.com/rest/v1/satellite/above/41.702/-76.014/0/70/18/&apiKey=RFLDHD-2N265V-UFLW6Z-512K"
     start_url = "https://api.n2yo.com/rest/v1/satellite/above/"
-    apiKey = "/0/70/18/&apiKey=RFLDHD-2N265V-UFLW6Z-512K" # TODO Check other parameters now hardcoded like altitude (0)
-
+    # TODO Check other parameters now hardcoded like altitude (0)
+    apiKey = "/0/70/18/&apiKey=RFLDHD-2N265V-UFLW6Z-512K"
 
     reply = {}
     reply['ranking'] = []
@@ -34,34 +47,36 @@ def index(request):
         aboveData = requests.get(n2yoEndpoint)
 
         subarea_ranking = [
-                {"satName": "Landsat",
-                 "timeliness": 7,
-                 "suitability_to_weather_type": 3,
-                 "suitability_to_time_of_the_day": 8,
-                 "suitability_to_event_type": 5,
-                 "spatial_resolution": 2,
-                 "frequency_of_update": 6,
-                 "cost": 9,
-                 "data_quality": 4
-                },
-                {"satName": "Modis",
-                 "timeliness": 6,
-                 "suitability_to_weather_type": 3,
-                 "suitability_to_time_of_the_day": 8,
-                 "suitability_to_event_type": 5,
-                 "spatial_resolution": 2,
-                 "frequency_of_update": 6,
-                 "price": 9,
-                 "data_quality": 4
-                }
-            ] # TODO ranking_algorithm(subarea, aboveData, localData)
+            {"satName": "Landsat",
+             "timeliness": 7,
+             "suitability_to_weather_type": 3,
+             "suitability_to_time_of_the_day": 8,
+             "suitability_to_event_type": 5,
+             "spatial_resolution": 2,
+             "frequency_of_update": 6,
+             "cost": 9,
+             "data_quality": 4
+             },
+            {"satName": "Modis",
+             "timeliness": 6,
+             "suitability_to_weather_type": 3,
+             "suitability_to_time_of_the_day": 8,
+             "suitability_to_event_type": 5,
+             "spatial_resolution": 2,
+             "frequency_of_update": 6,
+             "price": 9,
+             "data_quality": 4
+             }
+        ]  # TODO ranking_algorithm(subarea, aboveData, localData)
         reply['ranking'].insert(index, {
             'ranking_ord': "asc",
             'event_id': event_id,
             'aoi_id': aoi_id,
-            'sub_area_centroid': [subarea['centroid']['x'], subarea['centroid']['y']], # Retrieved from Group 5 API call
+            # Retrieved from Group 5 API call
+            'sub_area_centroid': [subarea['centroid']['x'], subarea['centroid']['y']],
             'geometry': subarea["geometry"],
-            'satlist': subarea_ranking # TODO stored in a "ranking" variable produced by the ranking algorithm, change it
+            # TODO stored in a "ranking" variable produced by the ranking algorithm, change it
+            'satlist': subarea_ranking
         })
 
     return HttpResponse(json.dumps(reply))
