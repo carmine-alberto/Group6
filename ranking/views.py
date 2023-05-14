5b5ab9da5180fdcda554edbf23134d537edb8961
olmarae <71841489+olmarae@users.noreply.github.com>
2023-05-14 18:29:31 +0200

    Add files via upload

--- ranking/views.py
+++ ranking/views.py
@@ -5,6 +5,12 @@ from django.http import JsonResponse
 import requests
 import json
 
+ #SATELLITES IMPORTS
+ #pyorbital.orbital TLE is missing - TODO check how to handle that
+from pyorbital import tlefile
+from datetime import datetime
+from math import sqrt
+
 
 def index(request):
     event_id = request.GET.get('event_id')
@@ -76,5 +82,68 @@ def index(request):
             # TODO stored in a "ranking" variable produced by the ranking algorithm, change it
             'satlist': subarea_ranking
         })
+        
 
     return HttpResponse(json.dumps(reply))
+
+#REQUIRES
+#satelliteData
+tle_url = "https://tle.ivanstanojevic.me/api/tle/"
+sat_id = "43638"
+tleEndpoint = tle_url + sat_id
+satelliteData = requests.get(tleEndpoint)
+#print(satelliteData)
+print(type(satelliteData))
+print(satelliteData.json()["name"])
+print(satelliteData.get("name"))
+#satelliteData DICTIONARY
+
+## key1: name
+## key2: line1
+## key3: line2
+## key4: minimumSnapshotArea
+## key5: orbitDuration
+""" 
+#targetLocation - API request to group 5 to obtain the event location
+group5_url = "https://group5/api/" #temporarly
+event_location_data = requests.get()
+## key1: lat
+## key2: lon
+## key3: alt
+
+#apiInfo - API request to group 7 to obtain the information about API provider for each satellite
+group7_url = "https://group7/api/" #temporarly
+
+""" 
+
+""" 
+
+def predictionAlgorithm(satelliteData, targetLocation):
+    # Specify the TLE data for the satellite 
+    tle_data = (
+        satelliteData["name"],
+        satelliteData["line1"],
+        satelliteData["line2"]
+    )        
+    tle = Tle(*tle_data) #TODO FIX
+
+    # Specify the desired location
+    lat = targetLocation["lat"] # New York City latitude
+    lon = targetLocation["lon"] # New York City longitude
+    alt = targetLocation["alt"] # Altitude in km
+
+    # Calculate the time required for the satellite to reach the specified location
+    dt = 300 # Time interval in seconds - TODO What is the LSB we want to use?
+    time = datetime.utcnow()
+
+    dt_per_orbit = orbitDuration / dt
+    
+    while dt_per_orbit
+        pos, _ = tle.get_position(time)
+        dist = pos.distance_from(lat, lon, alt)
+        # Here, we should consider the area size too. If it's too large, there could be some pieces missing      
+        if dist < sqrt(minimumSnapshotArea): # If satellite is within 5 km of the location, break out of the loop - TODO the threshold is selected by picking the square root of the minimum snapshot area
+            break
+        time += timedelta(seconds=dt)
+
+        """
