b2f248a401f682060b9f7805b4311c82f631774f
olmarae <olmararranze@gmail.com>
2023-05-18 18:55:18 +0200

    some errors solved

--- ranking/main.py
+++ ranking/main.py
@@ -9,7 +9,7 @@ from ranking.localdata import master_satellites
 from ranking.ranking import rank_satellites
 from ranking.timeliness import calculate_travel_time_and_orbit_duration
 
-MINIMUM_TIME_BETWEEN_EVENTS = 300
+MINIMUM_TIME_BETWEEN_EVENTS = 10
 
 rankings = []
 def get_rankings():
@@ -21,7 +21,7 @@ def loop_func():
     while True:
         #TODO Update with actual API
         group5_url = "https://group5/api/"  # temporarly
-        response_location_data = requests.get(group5_url)
+        #response_location_data = requests.get(group5_url)
 
 
         # if reply is valid
@@ -109,7 +109,7 @@ def loop_func():
                     satellite["line2"] = satellite_data["line2"]
 
                     #Attach estimatedTravelTime to each satellite
-                    (satellite["travelTime"], satellite_data["orbitDuration"]) = \
+                    satellite["travelTime"], satellite["orbitDuration"] = \
                         calculate_travel_time_and_orbit_duration(satellite, target_location, timestamp)
 
 
