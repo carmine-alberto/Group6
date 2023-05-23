4bef15235c3968f5d3f95413115f61af737ed868
olmarae <olmararranze@gmail.com>
2023-05-23 18:27:50 +0200

    csrf and input check

--- ranking/views.py
+++ ranking/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 import json
+import dateutil
 
+from django.views.decorators.csrf import csrf_exempt
 
 #Local imports
 from ranking.utils import parse_body
@@ -11,6 +13,17 @@ from ranking.main import create_ranking
 
 rankings = []
 
+def validate_date(date):
+    try:
+        dateutil.parser.parse(date)
+        return True
+    except Exception as e:
+        return False
+
+
+
+
+@csrf_exempt
 def index(request):
     if request.method == "GET":
         if request.method == "GET":
@@ -24,6 +37,22 @@ def index(request):
         #TODO sanitize input
 
         reply = {}
+
+        #Input checks on query parameters
+        ''''''
+        if int(aoi_id)<1 or int(aoi_id)>999:
+            reply['ranking'] = "The provided ID tuple doesn't have the correct format: the aoi has not a correct id"
+
+        event_id_sep=event_id.split(sep='-')
+        if event_id_sep[0]!="gr1" and event_id_sep[0]!="gr2":
+            reply['ranking'] = "The provided ID tuple doesn't have the correct format: the group is wrong"
+        elif event_id_sep[1].isalpha()==False:
+            reply['ranking'] = "The provided ID tuple doesn't have the correct format: the country is wrong"
+        elif validate_date(event_id_sep[2])==False or validate_date(event_id_sep[3])==False:
+            reply['ranking'] = "The provided ID tuple doesn't have the correct format: the date-time is wrong"
+        elif event_id_sep[4].isalpha()==False: 
+            reply['ranking'] = "The provided ID tuple doesn't have the correct format: the event type is wrong"
+
         ranking_exists = False
         for ranking in rankings:
             if ranking["id"]["event_id"] == event_id and ranking["id"]["aoi_id"] == aoi_id:
